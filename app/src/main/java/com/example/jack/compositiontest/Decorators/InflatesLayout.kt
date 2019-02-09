18713f5280a0f45410a64077e0c891da95a962fc
Jack Bradshaw <jack.bradshaw88@gmail.com>
2019-02-09 12:04:13 +0000

    InflatesLayout

---
+++ app/src/main/java/com/example/jack/compositiontest/Decorators/InflatesLayout.kt
@@ -0,0 +1,13 @@
+package com.example.jack.compositiontest.Decorators
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jack.compositiontest.FragmentDelegate
+
+class InflatesLayout(private val resId: Int): FragmentDelegate {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(resId, container, false)
+    }
+}
--- app/src/main/java/com/example/jack/compositiontest/FragmentOne.kt
+++ app/src/main/java/com/example/jack/compositiontest/FragmentOne.kt
@@ -1,18 +1,7 @@
 package com.example.jack.compositiontest
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.jack.compositiontest.Decorators.InflatesLayout
 
 class FragmentOne : DelegatingFragment() {
-    override val delegate = object : FragmentDelegate {
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.fragment_one, container, false)
-        }
-    }
+    override val delegate = InflatesLayout(R.layout.fragment_one)
 }
